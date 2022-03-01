44009d04ffedd3309c932d64d5648c7d1ae80d1a
Daniel Rosa <dan.rosa10@gmail.com>
2022-03-01 19:43:10 -0300

    teste em test

--- TSPNet/test_scripts/test_sign_local.py
+++ TSPNet/test_scripts/test_sign_local.py
@@ -52,8 +52,8 @@ def main(args, init_distributed=False):
     task = tasks.setup_task(args)
 
     # Load valid dataset (we load training data below, based on the latest checkpoint)
-    for valid_sub_split in args.valid_subset.split(','):
-        task.load_dataset(valid_sub_split, combine=False, epoch=0)
+    #for valid_sub_split in args.valid_subset.split(','):
+    #    task.load_dataset(valid_sub_split, combine=False, epoch=0)
 
     # Build model and criterion
     model = task.build_model(args)
