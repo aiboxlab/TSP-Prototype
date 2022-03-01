c005b1b3b128d9f0b18ffb792c26d002be857a8c
Daniel Rosa <dan.rosa10@gmail.com>
2022-03-01 17:02:10 -0300

    numheads

--- TSPNet/fairseq/modules/multihead_attention.py
+++ TSPNet/fairseq/modules/multihead_attention.py
@@ -40,11 +40,11 @@ class MultiheadAttention(nn.Module):
         self.vdim = vdim if vdim is not None else embed_dim
         self.qkv_same_dim = self.kdim == embed_dim and self.vdim == embed_dim
 
-        self.num_heads = 4
+        self.num_heads = num_heads
         self.dropout = dropout
-        self.head_dim = embed_dim // 4
+        self.head_dim = embed_dim // num_heads
         assert (
-            self.head_dim * 4 == self.embed_dim
+            self.head_dim * num_heads == self.embed_dim
         ), "embed_dim must be divisible by num_heads"
         self.scaling = self.head_dim ** -0.5
 
