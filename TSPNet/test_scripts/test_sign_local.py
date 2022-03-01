e71814f125e64ace7ee5abbfbd14471ee2a5b1c6
Daniel Rosa <dan.rosa10@gmail.com>
2022-03-01 19:41:53 -0300

    teste em test

--- TSPNet/test_scripts/test_sign_local.py
+++ TSPNet/test_scripts/test_sign_local.py
@@ -52,8 +52,8 @@ def main(args, init_distributed=False):
     task = tasks.setup_task(args)
 
     # Load valid dataset (we load training data below, based on the latest checkpoint)
-    #for valid_sub_split in args.valid_subset.split(','):
-    #    task.load_dataset(valid_sub_split, combine=False, epoch=0)
+    for valid_sub_split in args.valid_subset.split(','):
+        task.load_dataset(valid_sub_split, combine=False, epoch=0)
 
     # Build model and criterion
     model = task.build_model(args)
@@ -84,6 +84,7 @@ def main(args, init_distributed=False):
     valid_subsets = args.valid_subset.split(',')
 
     tokenize = sacrebleu.DEFAULT_TOKENIZER if not args.eval_tokenized_bleu else 'none'
+    hyps, refs = validate(args, trainer, task, epoch_itr, valid_subsets)
     
     '''
     hyps, refs = validate(args, trainer, task, epoch_itr, valid_subsets)
