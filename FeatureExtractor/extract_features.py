5f7664b4ff6c5d8bbd58411ad499e876311bdb8b
Daniel Rosa <dan.rosa10@gmail.com>
2022-03-01 21:23:10 -0300

    teste em test

--- FeatureExtractor/extract_features.py
+++ FeatureExtractor/extract_features.py
@@ -116,10 +116,10 @@ def run(weight, video, outroot, inp_channels='rgb'):
 
         text = "[{\"ident\": \""+ video +"\", \"size\": "+ str(len(features)) +"}]"
 
-        f = open("../TSPNet/data-bin/phoenix2014T/sp25000/test.sign-de.sign", "w")
+        f = open("TSPNet/data-bin/phoenix2014T/sp25000/test.sign-de.sign", "w")
         f.write(text)
         f.close()
 
-        f = open("../TSPNet/data-bin/phoenix2014T/sp25000/test.sign-de.de", "w")
+        f = open("TSPNet/data-bin/phoenix2014T/sp25000/test.sign-de.de", "w")
         f.write("Foo")
         f.close()
