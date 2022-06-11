f5c5304460528300ca8345c8a6944b94cf0cb916
Daniel Rosa <dan.rosa10@gmail.com>
2022-06-10 21:51:11 -0300

    colocando squeezenet em arquivo py

--- FeatureExtractor/extract_features.py
+++ FeatureExtractor/extract_features.py
@@ -5,23 +5,11 @@ import numpy as np
 import torch
 import cv2
 import datetime as dt
-import pickle
 from datetime import datetime
 from FeatureExtractor.models.pytorch_i3d import InceptionI3d
 
-
-# ==== AVG POOL ====
-class POOL(nn.Module):
-  def __init__(self):
-    super(POOL, self).__init__()
-
-    self.features = nn.Sequential(
-        nn.AvgPool2d(2, stride=1), # nn.AvgPool2d(2, stride=1)
-    )
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
-    x = self.features(x)
-    return x
-
+# ===== Squeeze =====
+from squeeze import squeeze_net
 
 # ===== Rescaling =====
 def crop_slices(image, height, width, slice_size):
@@ -159,42 +147,7 @@ def run(weight, video, outroot, inp_channels='rgb'):
         #print(name, len(features))
 
         # ===== Squeeze Avgpooling =====
-        video_mov = name + ".mov"
-        if video_mov in data.keys():
-            print("squeezing.")
-            obj = data[video_mov]
-            spanned_list = []
-            for i in range(len(obj)):
-                if i % span == 0:
-                    spanned_list.append(obj[i])
-
-            unified = []
-            for i in range(len(features)): # para cada i de features
-                tensor = features[i][0]
-
-                pkl_tensor = []
-                if i >= len(spanned_list):
-                    pkl_tensor = spanned_list[-1]
-                    overflow += 1
-                else:
-                    pkl_tensor = torch.tensor(spanned_list[i])
-
-                merged = torch.stack([ tensor[:960] , pkl_tensor ])
-                merged = torch.unsqueeze(merged, 0)
-                pooled = model(merged)
-
-                array = pooled[0][0]
-
-                concatenated = torch.cat((array, tensor[960:] ))
-                concatenated = torch.cat((concatenated, torch.tensor([0]) ))
-
-                result = torch.unsqueeze(concatenated, 0)
-
-                unified.append(result)
-
-            features = unified
-        else:
-            print("NAME ISNT IN KEYS!")
+        features = squeeze_net(name, features)
 
         torch.save(features, os.path.join(outdir, os.path.basename(video[:-4])) + '.pt')
 
---
+++ FeatureExtractor/squeeze.py
@@ -0,0 +1,59 @@
+import torch
+import pickle
+
+# ==== AVG POOL ====
+class POOL(nn.Module):
+    def __init__(self):
+        super(POOL, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.AvgPool2d(2, stride=1), # nn.AvgPool2d(2, stride=1)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        return x
+
+
+def squeeze_net(name, features):
+
+    model = POOL()
+    video_mov = name + ".mov"
+
+    if video_mov in data.keys():
+        print("squeezing.")
+        obj = data[video_mov]
+        spanned_list = []
+        for i in range(len(obj)):
+            if i % span == 0:
+                spanned_list.append(obj[i])
+
+        unified = []
+        for i in range(len(features)): # para cada i de features
+            tensor = features[i][0]
+
+            pkl_tensor = []
+            if i >= len(spanned_list):
+                pkl_tensor = spanned_list[-1]
+            else:
+                pkl_tensor = torch.tensor(spanned_list[i])
+
+            merged = torch.stack([ tensor[:960] , pkl_tensor ])
+            merged = torch.unsqueeze(merged, 0)
+            pooled = model(merged)
+
+            array = pooled[0][0]
+
+            concatenated = torch.cat((array, tensor[960:] ))
+            concatenated = torch.cat((concatenated, torch.tensor([0]) ))
+
+            result = torch.unsqueeze(concatenated, 0)
+
+            unified.append(result)
+
+        features = unified
+
+    else:
+        print("NAME ISNT IN KEYS!")
+
+    return features
