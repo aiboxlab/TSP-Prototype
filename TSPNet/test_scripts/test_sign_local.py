2f6a24061ca9d6327bcad3e349b417a295f4707b
Daniel Rosa <dan.rosa10@gmail.com>
2022-06-15 11:32:51 -0300

    testando output sem txt

--- TSPNet/test_scripts/test_sign_local.py
+++ TSPNet/test_scripts/test_sign_local.py
@@ -86,7 +86,8 @@ def main(args, init_distributed=False):
     tokenize = sacrebleu.DEFAULT_TOKENIZER if not args.eval_tokenized_bleu else 'none'
     hyps, refs = validate(args, trainer, task, epoch_itr, valid_subsets)
 
-    return hyps[0][0]
+    logger.info(hyps[0][0])
+    #return hyps[0][0]
 
     #print("SPLIT_HERE",hyps, refs)
     #f = open('./output.txt', 'w')
@@ -252,7 +253,7 @@ def cli_main(modify_parser=None):
         )
     else:
         # single GPU training
-        return main(args)
+        main(args)
 
 
 if __name__ == '__main__':
