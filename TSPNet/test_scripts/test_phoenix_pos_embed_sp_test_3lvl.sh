c2d347eac967ccef570977d879e56927ded08e6c
Danielgol <danielgol@yahoo.com.br>
2022-12-09 17:29:41 -0300

    teste

--- TSPNet/test_scripts/test_phoenix_pos_embed_sp_test_3lvl.sh
+++ TSPNet/test_scripts/test_phoenix_pos_embed_sp_test_3lvl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x -e -u -o pipefail
-CHECKPOINT=$(realpath './TSPNet/checkpoint_path/checkpoint_best.pt')
+CHECKPOINT=$(realpath '../checkpoint_path/checkpoint_best.pt')
 
 cd ..
 
@@ -10,7 +10,7 @@ PATIENCE=8
 ENC_LAYERS=1
 
 VOCAB=25000
-python ./TSPNet/test_scripts/test_sign_local.py data-bin/phoenix2014T/sp${VOCAB} \
+python test_scripts/test_sign_local.py data-bin/phoenix2014T/sp${VOCAB} \
         --task translation_sign \
         --target-lang pt \
         --max-tokens 4096 \
